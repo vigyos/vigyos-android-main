a164b2348e0714cc98dc8bc8f16587d9d393cde6
krishnkant <krishnkant.sharma@vigyos.com>
2023-08-08 18:29:19 +0530

    worked on profile page and api to services

---
+++ app/src/main/java/com/yoursuperidea/vigyos/Activity/ForgotPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.yoursuperidea.vigyos.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yoursuperidea.vigyos.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
